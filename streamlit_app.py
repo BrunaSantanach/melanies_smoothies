c845eccc6f5de017736363596ca031196ef686c8
BrunaSantanach <135543238+BrunaSantanach@users.noreply.github.com>
2025-06-06 13:14:34 +0200

    Update streamlit_app.py
    Bug corrected, dataframe name

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 #convertim amb taula pandas
-pd_df=my_data_frame.to_pandas()
+pd_df=my_dataframe.to_pandas()
 
 ## Els ingredients seleccionats els posem dins una vairable Ingredients
 
