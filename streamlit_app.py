88ab43ed8a32aa9fcec35de02fc98d97d947af4b
BrunaSantanach <135543238+BrunaSantanach@users.noreply.github.com>
2025-06-06 12:36:13 +0200

    Update streamlit_app.py
    .JSON response in API

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,5 +68,5 @@ if ingredients_list:
 # Paquet per utilitzar una api amb fruites
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
