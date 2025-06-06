568d101bd068084bf9f9617c1ba55a08c70e4e95
BrunaSantanach <135543238+BrunaSantanach@users.noreply.github.com>
2025-06-06 12:00:49 +0200

    Update streamlit_app.py
    summit button

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,3 +52,16 @@ if ingredients_list:
     #st.write (ingredients_string)
 
     ## Build a SQL Insert Statement & Test It, acaba donant el que posariem a el notebook, es pot veure amb ##st.write(my_insert_stmt)
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+   
+    st.write(my_insert_stmt)
+    
+    ## Butto sumit
+    time_to_insert=st.button ('Submit Order')
+    
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
