568ef89f12642c24592941c04d6bdbad8db62330
BrunaSantanach <135543238+BrunaSantanach@users.noreply.github.com>
2025-06-06 13:05:13 +0200

    Update streamlit_app.py
    Comprovació TAULA BUSQUEDA API

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,8 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection ("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ## Multiselect box
 
