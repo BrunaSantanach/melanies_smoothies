b4d04e6f8a30cc5b441eaee246a91bb3160823fb
BrunaSantanach <135543238+BrunaSantanach@users.noreply.github.com>
2025-06-06 11:16:05 +0200

    Create streamlit_app.py
    copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,53 @@
+## Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+## function named "col" we need to import it into our app
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.write(
+  """Choose the fruits you want in your custom Smoothie!
+  """
+)
+
+## Name box
+
+name_on_order = st.text_input('Name on Smoothie')
+st.write('The name on your Smoothie will be:', name_on_order)
+
+## Select box fruits
+#option = st.selectbox(
+#    "What is your favourite fruit?",
+#    ('Banana', 'Strawberries','Peaches'),)
+
+#st.write("Your favourite fruit is:", option)
+
+
+## Obrir la sessió per tenir les taules
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+## Multiselect box
+
+## Els ingredients seleccionats els posem dins una vairable Ingredients
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients:",
+    my_dataframe, 
+    max_selections=5
+)
+
+## Per mirar la llista, si no esà buida
+if ingredients_list:
+    
+     #st.write(ingredients_list)
+     #st.text(ingredients_list)
+    
+    ingredients_string=''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        
+    #st.write (ingredients_string)
+
+    ## Build a SQL Insert Statement & Test It, acaba donant el que posariem a el notebook, es pot veure amb ##st.write(my_insert_stmt)
